df21197affce9b08204dbdf81e3cb8771a85c5c3
Rajaa <kenbouchr2006@gmail.com>
2026-05-04 16:29:03 +0100

    .gitignore

--- db.php
+++ db.php
@@ -1,13 +1,24 @@
 <?php
-$host = 'localhost';
-$dbname = 'wama_cv';
-$username = 'root';
-$password = '';
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8",
+        $user,
+        $pass
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+
+} catch (PDOException $e) {
+    die("Erreur connexion DB : " . $e->getMessage());
 }
 ?>
